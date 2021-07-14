bc81fe70755326bb022552befed837bef990ab3b
Jhi97 <jeon9771@naver.com>
2021-07-14 15:26:28 +0900

    게시글 조회, 수정, 삭제 구현

--- src/main/java/com/jeon/board/Controller/BoardController.java
+++ src/main/java/com/jeon/board/Controller/BoardController.java
@@ -4,8 +4,10 @@ import com.jeon.board.dto.BoardDto;
 import com.jeon.board.service.BoardService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 //모든 필드를 인자값으로 하는 생성자를 @Autowired 없이 @AllArgsConstructor가 대신 생성하여 주입
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class BoardController {
     private BoardService boardService;
     @GetMapping("/")
-    public String list(){
+    public String list(Model model){
+        List<BoardDto> boardList = boardService.getBoardList();
+        model.addAttribute("boardList", boardList);
         return "board/list.html";
     }
 
@@ -25,6 +29,38 @@ public class BoardController {
     @PostMapping("/post")
     public String write(BoardDto boardDto){
         boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+    /*유동적으로 변하는 Path Variable을 처리하는 방법
+      URL 매핑하는 부분에서 {변수} 처리를 해주면, 메서드 파라미터로 @PathVariable("변수") 이렇게 받을 수 있음
+     */
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no, Model model){
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/detail.html";
+    }
+
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/update.html";
+    }
+
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDto){
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") Long no) {
+        boardService.deletePost(no);
 
         return "redirect:/";
     }
--- src/main/java/com/jeon/board/service/BoardService.java
+++ src/main/java/com/jeon/board/service/BoardService.java
@@ -1,11 +1,15 @@
 package com.jeon.board.service;
 
+import com.jeon.board.domain.entity.BoardEntity;
 import com.jeon.board.domain.repository.BoardRepository;
 import com.jeon.board.dto.BoardDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -13,7 +17,47 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     @Transactional
+    public List<BoardDto> getBoardList(){
+        List<BoardEntity> boardEntities = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for(BoardEntity boardEntity : boardEntities){
+            BoardDto boardDto = BoardDto.builder()
+                    .id(boardEntity.getId())
+                    .title(boardEntity.getTitle())
+                    .content(boardEntity.getContent())
+                    .writer(boardEntity.getWriter())
+                    .createdDate(boardEntity.getCreatedDate())
+                    .build();
+            boardDtoList.add(boardDto);
+        }
+
+        return boardDtoList;
+    }
+
+    @Transactional
     public Long savePost(BoardDto boardDto){
         return boardRepository.save(boardDto.toEntity()).getId();
     }
+
+    @Transactional
+    public BoardDto getPost(Long id){
+        Optional<BoardEntity> boardEntityWrapper = boardRepository.findById(id);
+        BoardEntity boardEntity = boardEntityWrapper.get();
+
+        BoardDto boardDto = BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .writer(boardEntity.getWriter())
+                .createdDate(boardEntity.getCreatedDate())
+                .build();
+        return boardDto;
+    }
+
+    @Transactional
+    public void deletePost(Long id){
+        boardRepository.deleteById(id);
+    }
+
 }
